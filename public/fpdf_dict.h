c1afc8b608674593b171c1905dafbd3a5ef70762
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-05-15 19:07:17 +0530

    Add root dictionary accessors
    This CL adds FPDF_GetRootDictionary to the public api in fpdf_dict.h as
    a wrapper around CPDF_Document::GetRoot(). This allows embedders to be
    able to access the root dictionary similarly to Pdf.Root in Pikepdf.
    
    Bug: 42270701
    Change-Id: I20c262a44c8cdaf26beaa039af2cb0ceb1d73ef6

--- public/fpdf_dict.h
+++ public/fpdf_dict.h
@@ -53,6 +53,18 @@ FPDF_EXPORT FPDF_DICTIONARY FPDF_CALLCONV
 FPDF_GetFontDictionary(FPDF_FONT font);
 
 // Experimental API.
+// Function: FPDF_GetRootDictionary
+//       Gets the root dictionary associated with a document object as an
+//       FPDF_DICTIONARY, which can be passed to helper functions to retrieve
+//       values associated with specific keys.
+// Parameters:
+//       doc - FPDF_DOCUMENT Handle to the document.
+// Return value:
+//       Returns an FPDF_DICTIONARY handle, or nullptr on failure.
+FPDF_EXPORT FPDF_DICTIONARY FPDF_CALLCONV
+FPDF_GetRootDictionary(FPDF_DOCUMENT doc);
+
+// Experimental API.
 // Function: FPDF_DictionaryGetString
 //       Gets a string value associated with a key from a dictionary.
 // Parameters:
