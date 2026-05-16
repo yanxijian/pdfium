2644335b18b534e7651df30d380536f2587a7004
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-05-16 12:09:15 +0530

    Part 2: Add Dictionary Accessors
    Change-Id: Ibbd61ac7be938cee8ab09ff3ed75e3a1cdb3aef0

--- fpdfsdk/fpdf_dict.cpp
+++ fpdfsdk/fpdf_dict.cpp
@@ -3,7 +3,9 @@
 // found in the LICENSE file.
 
 #include "public/fpdf_dict.h"
+#include "core/fpdfapi/parser/cpdf_dictionary.h"
 #include "core/fpdfapi/parser/cpdf_document.h"
+#include "core/fpdfapi/parser/cpdf_string.h"
 #include "fpdfsdk/cpdfsdk_helpers.h"
 
 extern "C" {
@@ -17,4 +19,80 @@ FPDF_GetPageDictionary(FPDF_DOCUMENT doc, int page_index) {
   return FPDFDictFromCPDFDict(document->GetPageDictionary(page_index).Get());
 }
 
+FPDF_EXPORT unsigned long FPDF_CALLCONV
+FPDF_DictionaryGetString(FPDF_DICTIONARY dictionary,
+                         FPDF_BYTESTRING key,
+                         void* buffer,
+                         unsigned long buflen) {
+  const CPDF_Dictionary* pDict = CPDFDictFromFPDFDict(dictionary);
+  if (!pDict || !key) {
+    return 0;
+  }
+  // SAFETY: required from caller.
+  return Utf16EncodeMaybeCopyAndReturnLength(
+      pDict->GetUnicodeTextFor(key),
+      UNSAFE_BUFFERS(SpanFromFPDFApiArgs(buffer, buflen)));
+}
+
+FPDF_EXPORT FPDF_RESULT_INT FPDF_CALLCONV
+FPDF_DictionaryGetInt(FPDF_DICTIONARY dictionary, FPDF_BYTESTRING key) {
+  const CPDF_Dictionary* dict = CPDFDictFromFPDFDict(dictionary);
+  if (!dict || !dict->KeyExist(key)) {
+    return {false, 0};
+  }
+  return {true, dict->GetIntegerFor(key)};
+}
+
+FPDF_EXPORT FPDF_RESULT_BOOL FPDF_CALLCONV
+FPDF_DictionaryGetBool(FPDF_DICTIONARY dictionary, FPDF_BYTESTRING key) {
+  const CPDF_Dictionary* dict = CPDFDictFromFPDFDict(dictionary);
+  if (!dict || !dict->KeyExist(key)) {
+    return {false, false};
+  }
+  return {true, dict->GetBooleanFor(key, false)};
+}
+
+FPDF_EXPORT FPDF_RESULT_FLOAT FPDF_CALLCONV
+FPDF_DictionaryGetFloat(FPDF_DICTIONARY dictionary, FPDF_BYTESTRING key) {
+  const CPDF_Dictionary* dict = CPDFDictFromFPDFDict(dictionary);
+  if (!dict || !dict->KeyExist(key)) {
+    return {false, 0.0f};
+  }
+  return {true, dict->GetFloatFor(key)};
+}
+
+FPDF_EXPORT FPDF_DICTIONARY FPDF_CALLCONV
+FPDF_DictionaryGetDict(FPDF_DICTIONARY dictionary, FPDF_BYTESTRING key) {
+  const CPDF_Dictionary* dict = CPDFDictFromFPDFDict(dictionary);
+  if (!dict) {
+    return nullptr;
+  }
+  return FPDFDictFromCPDFDict(dict->GetDictFor(key).Get());
+}
+
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDF_DictionaryGetRect(FPDF_DICTIONARY dictionary,
+                       FPDF_BYTESTRING key,
+                       FS_RECTF* rect) {
+  const CPDF_Dictionary* dict = CPDFDictFromFPDFDict(dictionary);
+  if (!dict) {
+    return false;
+  }
+  CFX_FloatRect r = dict->GetRectFor(key);
+  *rect = {r.left, r.top, r.right, r.bottom};
+  return true;
+}
+
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDF_DictionaryGetMatrix(FPDF_DICTIONARY dictionary,
+                         FPDF_BYTESTRING key,
+                         FS_MATRIX* matrix) {
+  const CPDF_Dictionary* dict = CPDFDictFromFPDFDict(dictionary);
+  if (!dict) {
+    return false;
+  }
+  CFX_Matrix m = dict->GetMatrixFor(key);
+  *matrix = {m.a, m.b, m.c, m.d, m.e, m.f};
+  return true;
+}
 }
