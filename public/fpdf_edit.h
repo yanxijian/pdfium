4c7240b3bcccefb6814544c9f681d0f2f304ff78
April Kallmeyer <ask@chromium.org>
2026-05-19 17:38:45 +0000

    [Ink Signatures] Create FDPFPageObj_AddExistingMark()
    CPDF_PageContentGenerator::ProcessContentMarks() already operates in
    such a way that consecutive page objects with the same
    CPDF_ContentMarkItem pointer get merged into a single BMC/EMC group.
    So this new API allows embedders to insert marks that span multiple page
    objects.
    
    Bug: 409021827, 408926609
    Change-Id: I139d279f0fd59ccd6ed05d0c1c79c560bfebb153

--- public/fpdf_edit.h
+++ public/fpdf_edit.h
@@ -465,6 +465,25 @@ FPDF_EXPORT FPDF_PAGEOBJECTMARK FPDF_CALLCONV
 FPDFPageObj_AddMark(FPDF_PAGEOBJECT page_object, FPDF_BYTESTRING name);
 
 // Experimental API.
+// Add an existing content mark to a |page_object|. If consecutive page objects
+// have the same |mark|, the generated PDF will contain a single mark that spans
+// all of them. If the page objects are not consecutive multiple copies of the
+// |mark| are inserted in the PDF.
+//
+//   page_object - handle to a page object.
+//   mark        - handle to a mark object.
+//
+// Returns true on success, or false on failure. The handles are all owned by
+// the library.
+//
+// Note: the |mark| param names use interned strings, so the memory for the
+// params is owned by the document passed to the set param functions. So that
+// original document must stay alive to add the |mark| to |page_object|.
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDFPageObj_AddExistingMark(FPDF_PAGEOBJECT page_object,
+                            FPDF_PAGEOBJECTMARK mark);
+
+// Experimental API.
 // Removes a content |mark| from a |page_object|.
 // The mark handle will be invalid after the removal.
 //
