77cb94473caafa56aaf2c95de4c7c034ba71014b
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-05-20 22:55:16 +0530

    Rename CidSet enum entries
    This CL renames the enum entries of the CidSet enum in cpdf_cidfont.h to
    follow kUpperCamelCase in line with the style guide and reduces the
    duplication of CIDSet by removing the trailing "CIDSET" prefix from the
    enum entry names.
    
    Change-Id: Ic9b7c41198158520af2e2046ffebc1d764ec2537

--- core/fpdfapi/font/cpdf_cidfont.h
+++ core/fpdfapi/font/cpdf_cidfont.h
@@ -20,13 +20,13 @@
 #include "core/fxcrt/unowned_ptr.h"
 
 enum CIDSet : uint8_t {
-  CIDSET_UNKNOWN,
-  CIDSET_GB1,
-  CIDSET_CNS1,
-  CIDSET_JAPAN1,
-  CIDSET_KOREA1,
-  CIDSET_UNICODE,
-  CIDSET_NUM_SETS
+  kUnknown,
+  kGB1,
+  kCNS1,
+  kJapan1,
+  kKorea1,
+  kUnicode,
+  kNumSets
 };
 
 struct CIDTransform {
@@ -95,7 +95,7 @@ class CPDF_CIDFont final : public CPDF_Font {
   bool cid_is_gid_ = false;
   bool ansi_widths_fixed_ = false;
   bool adobe_courier_std_ = false;
-  CIDSet charset_ = CIDSET_UNKNOWN;
+  CIDSet charset_ = kUnknown;
   int16_t default_width_ = 1000;
   int16_t default_vy_ = 880;
   int16_t default_w1_ = -1000;
