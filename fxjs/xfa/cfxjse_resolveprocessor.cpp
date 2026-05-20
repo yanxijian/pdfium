3b4e24a2e6cb4ba40070e3c169624b785feffe1b
Tom Sepez <tsepez@google.com>
2026-05-20 18:07:48 +0000

    Remove CFXJSE_Value wrapper in favor of v8::Global
    This change removes the CFXJSE_Value class, which was a thin wrapper
    around v8::Global<v8::Value>. Usages are replaced with v8::Global or
    v8::Local directly, simplifying the code. The use of unique_ptr<> to
    v8::Global can be reworked as a follow-on CL.
    
    - Removed all includes of cfxjse_value.h across the codebase.
    - Removed cfxjse_value.{h,cpp} and cfxjse_value_embeddertest.cpp from
      the BUILD.gn file.
    - Fixed V8 handle scope and context issues in cxfa_node.cpp that arose
      during replacement.
    - Moved FXJSE_ThrowMessage() to cfxjse_context.cpp.
    - Moved CFXJSE_Value::NewBoundFunction() to cfxjse_engine.cpp as a
      static helper.
    
    TAG=agy
    Bug: 42270615
    Change-Id: If2d31ace8fb16383b2cca8b66d15f8d53e9c6fbf

--- fxjs/xfa/cfxjse_resolveprocessor.cpp
+++ fxjs/xfa/cfxjse_resolveprocessor.cpp
@@ -14,9 +14,10 @@
 #include "core/fxcrt/check_op.h"
 #include "core/fxcrt/containers/contains.h"
 #include "core/fxcrt/fx_extension.h"
+#include "fxjs/fxv8.h"
 #include "fxjs/xfa/cfxjse_engine.h"
+#include "fxjs/xfa/cfxjse_isolatetracker.h"
 #include "fxjs/xfa/cfxjse_nodehelper.h"
-#include "fxjs/xfa/cfxjse_value.h"
 #include "fxjs/xfa/cjx_object.h"
 #include "xfa/fxfa/parser/cxfa_document.h"
 #include "xfa/fxfa/parser/cxfa_localemgr.h"
@@ -769,7 +770,18 @@ void CFXJSE_ResolveProcessor::DoPredicateFilter(v8::Isolate* pIsolate,
     CFXJSE_Context::ExecutionResult exec_result =
         engine_->RunScript(eLangType, wsExpression.AsStringView(),
                            pRnd->result_.objects[i - 1].Get());
-    if (!exec_result.status || !exec_result.value->ToBoolean(pIsolate)) {
+    if (!exec_result.status) {
+      pRnd->result_.objects.erase(pRnd->result_.objects.begin() + i - 1);
+      continue;
+    }
+    bool bool_val = false;
+    {
+      CFXJSE_ScopeUtil_IsolateHandleRootContext scope(pIsolate);
+      v8::Local<v8::Value> local_val =
+          v8::Local<v8::Value>::New(pIsolate, *exec_result.value);
+      bool_val = fxv8::ReentrantToBooleanHelper(pIsolate, local_val);
+    }
+    if (!bool_val) {
       pRnd->result_.objects.erase(pRnd->result_.objects.begin() + i - 1);
     }
   }
