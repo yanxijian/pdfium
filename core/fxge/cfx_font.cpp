65a11cca6b21a8096450a1ff4f70324e76fb010f
Tom Sepez <tsepez@google.com>
2026-05-26 23:18:36 +0000

    Move guts of CFX_CTTGSUBTable creation to CFX_Font.
    This low-level operations should not be mixed with CPDF objects.
    
    Bypass-Check-License: rename.
    Change-Id: I127c9ef93b2ae1ef7edf82640a7410fb74f7d01a

--- core/fxge/cfx_font.cpp
+++ core/fxge/cfx_font.cpp
@@ -15,10 +15,12 @@
 
 #include "build/build_config.h"
 #include "core/fxcrt/data_vector.h"
+#include "core/fxcrt/fixed_size_data_vector.h"
 #include "core/fxcrt/fx_codepage.h"
 #include "core/fxcrt/fx_stream.h"
 #include "core/fxcrt/span.h"
 #include "core/fxcrt/unowned_ptr.h"
+#include "core/fxge/cfx_cttgsubtable.h"
 #include "core/fxge/cfx_face.h"
 #include "core/fxge/cfx_fontmapper.h"
 #include "core/fxge/cfx_fontmgr.h"
@@ -173,6 +175,25 @@ CFX_Font::~CFX_Font() {
 #if BUILDFLAG(IS_APPLE)
   ReleasePlatformResource();
 #endif
+}
+
+std::unique_ptr<CFX_CTTGSUBTable> CFX_Font::ParseGSUBTable() const {
+  static constexpr uint32_t kGsubTag =
+      CFX_FontMapper::MakeTag('G', 'S', 'U', 'B');
+  if (!face_) {
+    return nullptr;
+  }
+  size_t length = face_->GetSfntTable(kGsubTag, {});
+  if (!length) {
+    return nullptr;
+  }
+
+  auto sub_data = FixedSizeDataVector<uint8_t>::Uninit(length);
+  if (!face_->GetSfntTable(kGsubTag, sub_data.span())) {
+    return nullptr;
+  }
+
+  return std::make_unique<CFX_CTTGSUBTable>(sub_data.span());
 }
 
 bool CFX_Font::LoadFaceZeroFromSpan(pdfium::span<const uint8_t> src_span,
