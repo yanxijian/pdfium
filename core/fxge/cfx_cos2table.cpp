472d9739eb6fcc8276255e631456bbdae600c03f
Tom Sepez <tsepez@google.com>
2026-05-27 23:51:44 +0000

    Add class to represent OS2 table
    Change-Id: I29080d9e1f92da206522e33de4e5ec2ecfaeb1fa

---
+++ core/fxge/cfx_cos2table.cpp
@@ -0,0 +1,37 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/fxge/cfx_cos2table.h"
+
+#include "core/fxcrt/byteorder.h"
+
+CFX_COS2Table::CFX_COS2Table() = default;
+
+CFX_COS2Table::CFX_COS2Table(pdfium::span<const uint8_t> data) {
+  if (data.size() < 42u) {
+    return;
+  }
+  panose_ = {data[32], data[33]};
+
+  if (data.size() < 58u) {
+    return;
+  }
+  usb_[0] = fxcrt::GetUInt32MSBFirst(data.subspan(42u, 4u).first<4u>());
+  usb_[1] = fxcrt::GetUInt32MSBFirst(data.subspan(46u, 4u).first<4u>());
+  usb_[2] = fxcrt::GetUInt32MSBFirst(data.subspan(50u, 4u).first<4u>());
+  usb_[3] = fxcrt::GetUInt32MSBFirst(data.subspan(54u, 4u).first<4u>());
+
+  if (data.size() < 86u) {
+    return;
+  }
+  csb_[0] = fxcrt::GetUInt32MSBFirst(data.subspan(78u, 4u).first<4u>());
+  csb_[1] = fxcrt::GetUInt32MSBFirst(data.subspan(82u, 4u).first<4u>());
+}
+
+CFX_COS2Table::CFX_COS2Table(std::array<uint32_t, 4> usb,
+                             std::array<uint32_t, 2> csb,
+                             std::array<uint8_t, 2> panose)
+    : usb_(usb), csb_(csb), panose_(panose) {}
+
+CFX_COS2Table::~CFX_COS2Table() = default;
---
+++ core/fxge/cfx_cos2table.h
@@ -0,0 +1,35 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FXGE_CFX_COS2TABLE_H_
+#define CORE_FXGE_CFX_COS2TABLE_H_
+
+#include <stdint.h>
+
+#include <array>
+
+#include "core/fxcrt/span.h"
+
+class CFX_COS2Table {
+ public:
+  CFX_COS2Table();
+  explicit CFX_COS2Table(pdfium::span<const uint8_t> data);
+  CFX_COS2Table(std::array<uint32_t, 4> usb,
+                std::array<uint32_t, 2> csb,
+                std::array<uint8_t, 2> panose);
+  ~CFX_COS2Table();
+
+  std::array<uint32_t, 4> GetUsb() const { return usb_; }
+  std::array<uint32_t, 2> GetCsb() const { return csb_; }
+  std::array<uint8_t, 2> GetPanose() const { return panose_; }
+
+  friend bool operator==(const CFX_COS2Table&, const CFX_COS2Table&) = default;
+
+ private:
+  std::array<uint32_t, 4> usb_ = {};
+  std::array<uint32_t, 2> csb_ = {};
+  std::array<uint8_t, 2> panose_ = {};
+};
+
+#endif  // CORE_FXGE_CFX_COS2TABLE_H_
