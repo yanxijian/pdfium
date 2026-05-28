9d41994903a7989b25ed273fc2628efe5cff71f6
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-05-28 11:47:53 +0530

    Add simple tests for Brotli compression
    This CL adds test based on brotli-stream compressed PDF files and updates the needed BUILD files to be able to allow for Brotli compression to be tested when the feature is built. These test files are based on existing files with a compressed stream using brotli.
    
    Bug: 475855993
    Change-Id: I8162dd98885cf6e75bfe8b5d82f61d7d4252bea1

---
+++ core/fxcodec/brotli/brotli_embeddertest.cpp
@@ -0,0 +1,50 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/embedder_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using BrotliEmbedderTest = EmbedderTest;
+
+// TODO(crbug.com/475855993): Enable for all files when BrotliDecode support is
+// added.
+TEST_F(BrotliEmbedderTest, BrotliWithAnnots) {
+  ASSERT_TRUE(OpenDocument("annots_brotli.pdf"));
+
+  ScopedPage page = LoadScopedPage(0);
+  ASSERT_TRUE(page);
+
+  ScopedFPDFBitmap bitmap = RenderLoadedPage(page.get());
+  ASSERT_TRUE(bitmap);
+}
+
+TEST_F(BrotliEmbedderTest, SimpleBrotliWithText) {
+  ASSERT_TRUE(OpenDocument("hello_world_brotli.pdf"));
+
+  ScopedPage page = LoadScopedPage(0);
+  ASSERT_TRUE(page);
+
+  ScopedFPDFBitmap bitmap = RenderLoadedPage(page.get());
+  ASSERT_TRUE(bitmap);
+}
+
+TEST_F(BrotliEmbedderTest, BrotliRectangles) {
+  ASSERT_TRUE(OpenDocument("rectangles_brotli.pdf"));
+
+  ScopedPage page = LoadScopedPage(0);
+  ASSERT_TRUE(page);
+
+  ScopedFPDFBitmap bitmap = RenderLoadedPage(page.get());
+  ASSERT_TRUE(bitmap);
+}
+
+TEST_F(BrotliEmbedderTest, BrotliWithLength1Argument) {
+  ASSERT_TRUE(OpenDocument("hello_world_brotli_with_length1.pdf"));
+
+  ScopedPage page = LoadScopedPage(0);
+  ASSERT_TRUE(page);
+
+  ScopedFPDFBitmap bitmap = RenderLoadedPage(page.get());
+  ASSERT_TRUE(bitmap);
+}
