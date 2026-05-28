348ab93ef0ee218a7a2acfa0c173362af3252121
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-05-28 11:47:53 +0530

    Add Simple Tests for Brotli Compression
    This CL adds test based on the PDF Association's samples files
    for Brotli compression to be able to test out the Brotli compression
    feature once it has been built.
    
    Bug: 475855993
    Change-Id: I8162dd98885cf6e75bfe8b5d82f61d7d4252bea1

---
+++ core/fxcodec/brotli/brotli_embeddertest.cpp
@@ -0,0 +1,23 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/embedder_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class BrotliEmbedderTest : public EmbedderTest {};
+
+// These Documents are samples from the PDF Association
+// TODO(crbug.com/475855993): Enable for all files when BrotliDecode support is
+// added.
+TEST_F(BrotliEmbedderTest, DISABLED_PrototypeA) {
+  ASSERT_TRUE(OpenDocument("brotli_prototype_a.pdf"));
+}
+
+TEST_F(BrotliEmbedderTest, DISABLED_PrototypeB) {
+  ASSERT_TRUE(OpenDocument("brotli_prototype_b.pdf"));
+}
+
+TEST_F(BrotliEmbedderTest, DISABLED_PrototypeC) {
+  ASSERT_TRUE(OpenDocument("brotli_prototype_c.pdf"));
+}
