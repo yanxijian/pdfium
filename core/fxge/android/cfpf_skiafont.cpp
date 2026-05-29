6ef3ae833e9380e4de043b5d5f05c2325bf29ab2
Tom Sepez <tsepez@google.com>
2026-05-29 20:28:29 +0000

    [Android] Nest font metadata privately in manager
    Nest Entry privately in CFPF_SkiaFontMgr and pass CFX_Face directly to
    CFPF_SkiaFont. Delete cfpf_skiapathfont.h and cfpf_skiapathfont.cpp.
    
    Bypass-Check-License: rename below in CL chain.
    Change-Id: Icff26018b9e02c480415c68b442e425a49c46b96

--- core/fxge/android/cfpf_skiafont.cpp
+++ core/fxge/android/cfpf_skiafont.cpp
@@ -6,23 +6,12 @@
 
 #include "core/fxge/android/cfpf_skiafont.h"
 
-#include <algorithm>
+#include <utility>
 
-#include "core/fxcrt/fx_codepage.h"
-#include "core/fxcrt/fx_coordinates.h"
-#include "core/fxcrt/fx_system.h"
 #include "core/fxcrt/numerics/safe_conversions.h"
-#include "core/fxge/android/cfpf_skiafontmgr.h"
-#include "core/fxge/android/cfpf_skiapathfont.h"
-#include "core/fxge/fx_fontencoding.h"
 
-CFPF_SkiaFont::CFPF_SkiaFont(CFPF_SkiaFontMgr* font_mgr,
-                             const CFPF_SkiaPathFont* font,
-                             FX_Charset uCharset)
-    : font_mgr_(font_mgr),
-      font_(font),
-      face_(font_mgr_->GetFontFace(font_->path(), font_->face_index())),
-      charset_(uCharset) {}
+CFPF_SkiaFont::CFPF_SkiaFont(RetainPtr<CFX_Face> face, FX_Charset uCharset)
+    : face_(std::move(face)), charset_(uCharset) {}
 
 CFPF_SkiaFont::~CFPF_SkiaFont() = default;
 
