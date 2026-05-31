34a520439043b163979a359d3650fd440d0b8db8
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-05-31 16:58:50 +0530

    Add BrotliDecode filter support for PDF 2.0 streams
    This CL Adds brotli_decoder.cpp/.h implementing BrotliDecoder::Decode()
    to support the /BrotliDecode filter. This uses the one-shot Brotli API
    with buffer growth (until the max threshold) on
    BROTLI_DECODER_RESULT_NEEDS_MORE_OUTPUT.
    
    Wires the /BrotliDecode filter into fpdf_parser_decode.cpp and
    cpdf_streamparser.cpp alongside existing filters, and enables the Brotli
    embedder tests now that the decoder is in place.
    
    Bug: 475855993
    Change-Id: I6f1532d14fd547bc8e9efdb4d6c5d99605109133

---
+++ core/fxcodec/brotli/brotli_decoder.h
@@ -0,0 +1,25 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FXCODEC_BROTLI_BROTLI_DECODER_H_
+#define CORE_FXCODEC_BROTLI_BROTLI_DECODER_H_
+
+#include <stdint.h>
+
+#include "core/fxcodec/data_and_bytes_consumed.h"
+#include "core/fxcrt/span.h"
+
+class CPDF_Dictionary;
+typedef struct BrotliDecoderStateStruct BrotliDecoderState;
+
+class BrotliDecoder {
+ public:
+  struct BrotliDeleter {
+    void operator()(BrotliDecoderState* ptr) const;
+  };
+  static DataAndBytesConsumed Decode(pdfium::span<const uint8_t> src_span,
+                                     uint32_t estimated_decode_size);
+};
+
+#endif  // CORE_FXCODEC_BROTLI_BROTLI_DECODER_H_
