6f576d10953b3648bd5bc2ba6d1e244d5ae7bd72
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-06-11 20:59:51 +0530

    Add public API to enable/disable Brotli stream decoding
    This CL adds experimental public functions to enable or disable the
    decoding of Brotli-compressed data streams.
    
    This is done by exposing FPDF_SetBrotliDecodeEnabled() and
    FPDF_IsBrotliDecodeEnabled() in the public API in fpdfview.h, adding
    functions to brotli_decoder.cpp and .h to read and write to a boolean
    (g_brotli_enabled_) and using the getter of this bool to be able to
    support runtime changes to whether BrotliDecode is enabled (such as with
    chromium feature flags).
    
    Bug: 475855993
    Change-Id: I4ef555dfc58581bae0289c293d2f77a387749637

--- core/fxcodec/brotli/brotli_decoder.h
+++ core/fxcodec/brotli/brotli_decoder.h
@@ -21,6 +21,20 @@ class BrotliDecoder {
  public:
   static DataAndBytesConsumed Decode(pdfium::span<const uint8_t> src_span,
                                      uint32_t estimated_decode_size);
+<<<<<<< PATCH SET (c5db61f591fd127fb33e8773c8ee84510d47c2ee Add public API to enable/disable Brotli stream decoding)
+  static void SetBrotliEnabled(bool enabled);
+  static bool GetBrotliEnabled();
+ private:
+  struct BrotliDeleter {
+    void operator()(BrotliDecoderState* ptr) const;
+  };
+||||||| BASE      (85039431fb09ff95b0bf11c9c001c8da6534f18c Add BrotliDecode filter support for PDF 2.0 streams)
+ private:
+  struct BrotliDeleter {
+    void operator()(BrotliDecoderState* ptr) const;
+  };
+=======
+>>>>>>> BASE      (baca7d2d708d7dff0a6e16b5d8d7dd2851a1cbaf Add BrotliDecode filter support for PDF 2.0 streams)
 };
 
 #endif  // CORE_FXCODEC_BROTLI_BROTLI_DECODER_H_
