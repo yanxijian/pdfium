ac9b6129eb524c5cb8455526b703944ebdd59796
Tom Sepez <tsepez@google.com>
2026-06-11 19:39:18 +0000

    Prepare CFX_FolderFontInfo for Android Refactoring
    Add infrastructure changes to CFX_FolderFontInfo to support Android's
    font matching requirements in a subsequent CL.
    
    Changes:
    - Add glyph_count_ to FontFaceInfo.
    - Parse 'maxp' table in CFX_FolderFontInfo::ReportFace to extract
      glyph_count_ during scanning. This information is the crux of
      the change, allowing android to drop FreeType usage in next CL.
    - Add virtual IsBetterMatch hook to CFX_FolderFontInfo, replacing
      inline matching logic in FindFont.
    - Move internal file-reading and name-matching helpers in
      cfx_folderfontinfo.cpp to static class methods.
    
    TAG=agy
    CONV=71ddca6b-8a59-46ab-a366-5526235fb713
    
    Change-Id: I1480e3e0aafe45e1df307d5b88fdb026b018ad67

--- core/fxge/cfx_folderfontinfo.h
+++ core/fxge/cfx_folderfontinfo.h
@@ -65,6 +65,7 @@ class CFX_FolderFontInfo : public SystemFontInfoIface {
     const uint32_t file_size_;
     uint32_t styles_ = 0;
     Mask<FX_CharsetFlag> charsets_;
+    uint32_t glyph_count_ = 0;
   };
 
   void ScanPath(const ByteString& path);
@@ -80,6 +81,22 @@ class CFX_FolderFontInfo : public SystemFontInfoIface {
                  int pitch_family,
                  const ByteString& family,
                  bool bMatchName);
+  static bool FindFamilyNameMatch(ByteStringView family_name,
+                                  const ByteString& installed_font_name);
+  static ByteString ReadStringFromFile(FILE* pFile, uint32_t size);
+  static ByteString LoadTableFromTT(FILE* pFile,
+                                    const uint8_t* pTables,
+                                    uint32_t nTables,
+                                    uint32_t tag,
+                                    FX_FILESIZE fileSize);
+
+  virtual bool IsBetterMatch(const FontFaceInfo* candidate,
+                             int32_t candidate_score,
+                             const FontFaceInfo* current_best,
+                             int32_t current_best_score,
+                             FX_Charset charset,
+                             const ByteString& family,
+                             bool bMatchName) const;
 
   std::map<ByteString, std::unique_ptr<FontFaceInfo>> font_list_;
   std::vector<ByteString> path_list_;
