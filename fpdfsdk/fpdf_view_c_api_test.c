9d565abd0d7f6de19b7dadfa7afadb48753fb885
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-06-11 20:59:51 +0530

    Add public API to enable/disable Brotli stream decoding
    This CL adds experimental public functions to enable or disable the
    decoding of Brotli-compressed data streams.
    
    This is done by exposing FPDF_SetBrotliDecodeEnabled() and
    FPDF_IsBrotliDecodeEnabled() in the public API in fpdfview.h, adding
    functions to brotli_decoder.cpp and .h to read and write to a boolean
    (g_brotli_enabled_) and using the getter of this bool to be able to
    support runtime changes to whether BrotliDecode is enabled (such as with
    chromium feature flags).
    
    Bug: 475855993
    Change-Id: I4ef555dfc58581bae0289c293d2f77a387749637

--- fpdfsdk/fpdf_view_c_api_test.c
+++ fpdfsdk/fpdf_view_c_api_test.c
@@ -539,6 +539,9 @@ int CheckPDFiumCApi() {
     CHK(FPDF_GetXFAPacketName);
     CHK(FPDF_InitLibrary);
     CHK(FPDF_InitLibraryWithConfig);
+#ifdef PDF_ENABLE_BROTLI
+    CHK(FPDF_IsBrotliDecodeEnabled);
+#endif
     CHK(FPDF_LoadCustomDocument);
     CHK(FPDF_LoadDocument);
     CHK(FPDF_LoadMemDocument);
@@ -552,6 +555,9 @@ int CheckPDFiumCApi() {
     CHK(FPDF_RenderPageBitmapWithMatrix);
 #if defined(PDF_USE_SKIA)
     CHK(FPDF_RenderPageSkia);
+#endif
+#ifdef PDF_ENABLE_BROTLI
+    CHK(FPDF_SetBrotliDecodeEnabled);
 #endif
 #if defined(_WIN32)
     CHK(FPDF_SetPrintMode);
