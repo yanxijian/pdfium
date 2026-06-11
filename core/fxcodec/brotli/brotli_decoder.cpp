550aa79d2a14196094aa419009d9e54f9eba30d2
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-06-11 20:59:51 +0530

    Add public API to enable/disable Brotli stream decoding
    This CL adds experimental public functions to enable or disable the
    decoding of Brotli-compressed data streams.
    
    This is done by exposing FPDF_SetBrotliDecodeEnabled() and
    FPDF_IsBrotliDecodeEnabled() in the public API in fpdfview.h, adding
    functions to brotli_decoder.cpp and .h to read and write to a boolean
    (g_brotli_enabled_) and using the getter of this bool to be able to
    support runtime changes to whether BrotliDecode is enabled (such as with
    chromium feature flags).
    
    Bug: 475855993
    Change-Id: I4ef555dfc58581bae0289c293d2f77a387749637

--- core/fxcodec/brotli/brotli_decoder.cpp
+++ core/fxcodec/brotli/brotli_decoder.cpp
@@ -15,6 +15,7 @@
 
 namespace {
 constexpr size_t kMaxDecodeBytes = 64 * 1024 * 1024;
+bool g_brotli_enabled = false;
 
 struct BrotliDecoderStateDeleter {
   void operator()(struct BrotliDecoderStateStruct* ptr) const;
@@ -28,6 +29,7 @@ void BrotliDecoderStateDeleter::operator()(
 
 DataAndBytesConsumed BrotliDecoder::Decode(pdfium::span<const uint8_t> src_span,
                                            uint32_t estimated_decode_size) {
+  CHECK(g_brotli_enabled);
   if (src_span.empty()) {
     return {DataVector<uint8_t>(), 0u};
   }
@@ -69,4 +71,12 @@ DataAndBytesConsumed BrotliDecoder::Decode(pdfium::span<const uint8_t> src_span,
     }
     return {DataVector<uint8_t>(), 0u};
   }
+}
+
+void BrotliDecoder::SetBrotliEnabled(bool enabled) {
+  g_brotli_enabled = enabled;
+}
+
+bool BrotliDecoder::GetBrotliEnabled() {
+  return g_brotli_enabled;
 }
