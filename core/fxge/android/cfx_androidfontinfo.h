06b42cb2bc34ffcc55eba11aaa02e2a1263acc67
Tom Sepez <tsepez@google.com>
2026-06-15 17:41:07 +0000

    [Android] Fold Skia Font Manager into CFX_AndroidFontInfo
    Refactor CFX_AndroidFontInfo to inherit from CFX_FolderFontInfo
    instead of SystemFontInfoIface, centralizing directory scanning and
    lazy font matching. This allows Android to drop FreeType usage during
    startup scanning, significantly improving startup time.
    
    Android-specific heuristics are preserved:
      - Override IsBetterMatch: Retains Android's CJK matching logic
        (preferring the font with more glyphs on fallback, using the glyph
        count extracted from 'maxp' in the previous CL).
      - Roboto/Droid substitutions and Arabic/Symbol overrides are moved
        from CFPF_SkiaFontMgr to CFX_AndroidFontInfo.
    
    Cleanups and Improvements:
      - Deleted stale cfpf_skiafont* and cfpf_skiafontmgr* files.
    
    TAG=agy
    Change-Id: I9f827495ddd6d3540f1cf7b267d6725767670f8b

--- core/fxge/android/cfx_androidfontinfo.h
+++ core/fxge/android/cfx_androidfontinfo.h
@@ -7,54 +7,32 @@
 #ifndef CORE_FXGE_ANDROID_CFX_ANDROIDFONTINFO_H_
 #define CORE_FXGE_ANDROID_CFX_ANDROIDFONTINFO_H_
 
-#include <stdint.h>
+#include "core/fxge/cfx_folderfontinfo.h"
 
-#include "core/fxcrt/bytestring.h"
-#include "core/fxcrt/span.h"
-#include "core/fxcrt/unowned_ptr.h"
-#include "core/fxge/cfx_fontmapper.h"
-#include "core/fxge/systemfontinfo_iface.h"
-
-class CFPF_SkiaFontMgr;
-
-class CFX_AndroidFontInfo final : public SystemFontInfoIface {
+class CFX_AndroidFontInfo final : public CFX_FolderFontInfo {
  public:
   CFX_AndroidFontInfo();
   ~CFX_AndroidFontInfo() override;
 
-  bool Init(CFPF_SkiaFontMgr* font_mgr, const char** user_paths);
+  bool Init(const char** user_paths);
 
   // SystemFontInfoIface:
-  void EnumFontList(CFX_FontMapper* pMapper) override;
   void* MapFont(int weight,
                 bool bItalic,
                 FX_Charset charset,
                 int pitch_family,
                 const ByteString& face) override;
-  void* GetFont(const ByteString& face) override;
-  size_t GetFontData(void* hFont,
-                     uint32_t table,
-                     pdfium::span<uint8_t> buffer) override;
-  bool GetFaceName(void* hFont, ByteString* name) override;
-  bool GetFontCharset(void* hFont, FX_Charset* charset) override;
-  void DeleteFont(void* hFont) override;
 
- private:
-  UnownedPtr<CFPF_SkiaFontMgr> font_mgr_;
+ protected:
+  // CFX_FolderFontInfo:
+
+  bool IsBetterMatch(const FontFaceInfo* candidate,
+                     int32_t candidate_score,
+                     const FontFaceInfo* current_best,
+                     int32_t current_best_score,
+                     FX_Charset charset,
+                     const ByteString& family,
+                     bool bMatchName) const override;
 };
-
-struct SkiaFontMap {
-  uint32_t family;
-  const char* subst;
-};
-
-pdfium::span<const SkiaFontMap> GetSkiaFontmap();
-pdfium::span<const SkiaFontMap> GetSkiaSansFontMap();
-
-uint32_t SkiaNormalizeFontName(ByteStringView family);
-const char* SkiaGetSubstFont(uint32_t hash,
-                             pdfium::span<const SkiaFontMap> font_map);
-bool SkiaMaybeArabic(ByteStringView facename);
-bool SkiaMaybeSymbol(ByteStringView facename);
 
 #endif  // CORE_FXGE_ANDROID_CFX_ANDROIDFONTINFO_H_
