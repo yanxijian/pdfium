62b5338ea6353f1b607c59050a2bee8dde134f70
Seung Hyun Jin <seunghyunjin@google.com>
2026-06-16 21:47:54 +0000

    Refactor/modernize CPVT_Word and CPVT_WordInfo structs
    Replace manual constructor initializer lists with inline default
    member initializers, and explicitly declares default and copy
    constructors, and assignment operators. Purely structural
    refactor, no functional or logical changes.
    
    Bug: 40115028
    Change-Id: Id434c4d4988be9e9ae800774d3cb79351c15adf5

---
+++ core/fpdfdoc/cpvt_word.cpp
@@ -0,0 +1,12 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
+
+#include "core/fpdfdoc/cpvt_word.h"
+
+CPVT_Word::CPVT_Word() = default;
+CPVT_Word::CPVT_Word(const CPVT_Word&) = default;
+CPVT_Word& CPVT_Word::operator=(const CPVT_Word&) = default;
+CPVT_Word::~CPVT_Word() = default;
