7bb65ad3ab7810b16284b5637ca455a7866f1d0f
Tom Sepez <tsepez@google.com>
2026-06-16 22:48:15 +0000

    Refactor CPDF_SimpleFont hierarchy to introduce CPDF_FaceBasedSimpleFont.
    This moves the FreeType/Fontations-backed metrics caching and glyph
    mapping tables out of CPDF_SimpleFont into a new intermediate
    CPDF_FaceBasedSimpleFont class. This saves ~6 KB of memory per
    CPDF_Type3Font instance, as Type3 fonts do not use a physical font
    face and thus do not need these tables.
    
    TAG=agy
    CONV=22aaba7e-d459-4a6f-8189-64ba31f20dc2
    
    Change-Id: I5c62c3a3e91a9733d9ad89d42f47f0fce96c2336

--- core/fpdfapi/font/cpdf_type3font.h
+++ core/fpdfapi/font/cpdf_type3font.h
@@ -34,6 +34,7 @@ class CPDF_Type3Font final : public CPDF_SimpleFont {
   void WillBeDestroyed() override;
   int GetCharWidth(uint32_t charcode) override;
   FX_RECT GetCharBBox(uint32_t charcode) override;
+  int GlyphFromCharCode(uint32_t charcode, bool* pVertGlyph) override;
 
   void SetPageResources(CPDF_Dictionary* pResources) {
     page_resources_.Reset(pResources);
@@ -50,9 +51,6 @@ class CPDF_Type3Font final : public CPDF_SimpleFont {
 
   // CPDF_Font:
   bool Load() override;
-
-  // CPDF_SimpleFont:
-  void LoadGlyphMap() override;
 
   // The depth char loading is in, to avoid recurive calling LoadChar().
   int char_loading_depth_ = 0;
