742775bae5df6b518436f898a7a19e2c38735961
Tom Sepez <tsepez@google.com>
2026-06-16 17:50:59 +0000

    Refactor CPDF_SimpleFont hierarchy to introduce CPDF_FaceBasedSimpleFont.
    This moves the FreeType/Fontations-backed metrics caching and glyph
    mapping tables out of CPDF_SimpleFont into a new intermediate
    CPDF_FaceBasedSimpleFont class. This saves ~6 KB of memory per
    CPDF_Type3Font instance, as Type3 fonts do not use a physical font
    face and thus do not need these tables.
    
    TAG=agy
    CONV=22aaba7e-d459-4a6f-8189-64ba31f20dc2
    
    Change-Id: I5c62c3a3e91a9733d9ad89d42f47f0fce96c2336

--- core/fpdfapi/font/cpdf_simplefont.h
+++ core/fpdfapi/font/cpdf_simplefont.h
@@ -9,7 +9,6 @@
 
 #include <stdint.h>
 
-#include <array>
 #include <vector>
 
 #include "core/fpdfapi/font/cpdf_font.h"
@@ -17,22 +16,12 @@
 #include "core/fxcrt/fx_string.h"
 
 // 8-bit fonts, supporting at most 256 characters mapped via /Encoding.
-// See ISO 32000-1:2008, section 9.6
 class CPDF_SimpleFont : public CPDF_Font {
  public:
   ~CPDF_SimpleFont() override;
 
   // CPDF_Font
-  int GetCharWidth(uint32_t charcode) override;
-  FX_RECT GetCharBBox(uint32_t charcode) override;
-  int GlyphFromCharCode(uint32_t charcode, bool* pVertGlyph) override;
   bool IsUnicodeCompatible() const override;
-  WideString UnicodeFromCharCode(uint32_t charcode) const override;
-  uint32_t CharCodeFromUnicode(wchar_t Unicode) const override;
-
-  const CPDF_FontEncoding* GetEncoding() const { return &encoding_; }
-
-  bool HasFontWidths() const override;
 
   static constexpr char kNotDef[] = ".notdef";
   static constexpr char kSpace[] = "space";
@@ -45,20 +34,11 @@ class CPDF_SimpleFont : public CPDF_Font {
 
   virtual void LoadGlyphMap() = 0;
 
-  bool LoadCommon();
-  void LoadSubstFont();
-  void LoadCharMetrics(int charcode);
-  void LoadCharWidths(const CPDF_Dictionary* font_desc);
   void LoadDifferences(const CPDF_Dictionary* encoding);
   void LoadPDFEncoding(bool bEmbedded, bool bTrueType);
 
-  CPDF_FontEncoding encoding_{FontEncoding::kBuiltin};
   FontEncoding base_encoding_ = FontEncoding::kBuiltin;
-  bool use_font_width_ = false;
   std::vector<ByteString> char_names_;
-  std::array<uint16_t, kInternalTableSize> glyph_index_;
-  std::array<uint16_t, kInternalTableSize> char_width_;
-  std::array<FX_RECT, kInternalTableSize> char_bbox_;
 };
 
 #endif  // CORE_FPDFAPI_FONT_CPDF_SIMPLEFONT_H_
