bb4a146e67fa638b3bb416f53c9e934bdbb2f0f4
Tom Sepez <tsepez@google.com>
2026-06-18 19:55:03 +0000

    Refactor Skrifa integration to use member methods.
    Migrate from free functions that take font data slices to member
    methods on SkrifaFont. This allows caching the parsed font state and
    prevents repeated passing of raw data slices across the FFI boundary.
    
    Update CFX_Face to manage the SkrifaFont lifecycle and delegate
    queries to it.
    
    TAG=agy
    CONV=53beacdb-cb1c-42a8-ae3f-b599eabe37b6
    
    Change-Id: Ib0f280596579ba702194e3bb73e5b7cd2ac8d32e

--- core/fxge/cfx_face.h
+++ core/fxge/cfx_face.h
@@ -24,11 +24,10 @@
 
 #if defined(PDF_USE_SKIA)
 #include "third_party/skia/include/core/SkRefCnt.h"  // nogncheck
-#endif
-
 #if defined(PDF_ENABLE_FONTATIONS)
 #include "third_party/rust/cxx/v1/cxx.h"
-#endif
+#endif  // defined(PDF_ENABLE_FONTATIONS)
+#endif  // defined(PDF_USE_SKIA)
 
 class CFX_CTTGSUBTable;
 class CFX_GlyphBitmap;
@@ -45,6 +44,8 @@ class SkTypeface;
 
 #if defined(PDF_ENABLE_FONTATIONS)
 struct SkrifaFontHolder;
+#else
+struct SkrifaFontHolder {};
 #endif
 
 namespace fxge {
@@ -164,12 +165,8 @@ class CFX_Face final : public Retainable, public Observable {
  private:
   CFX_Face(RetainPtr<Retainable> cache_entry,
            RetainPtr<CFX_ReadOnlySpanStream> font_stream,
-           FT_FaceRec* rec
-#if defined(PDF_ENABLE_FONTATIONS)
-           ,
-           std::unique_ptr<SkrifaFontHolder> skrifa_font
-#endif
-  );
+           FT_FaceRec* rec,
+           std::unique_ptr<SkrifaFontHolder> skrifa_font);
 
   ~CFX_Face() override;
 
@@ -188,14 +185,6 @@ class CFX_Face final : public Retainable, public Observable {
   std::optional<std::array<uint8_t, 2>> GetOs2Panose();
 #endif
 
-#if defined(PDF_ENABLE_SKIA_TYPEFACE_CHECKS)
-  std::unique_ptr<CFX_Path> LoadGlyphPathFontations(
-      uint32_t glyph_index,
-      int dest_width,
-      bool is_vertical,
-      const CFX_SubstFont* subst_font);
-#endif
-
   // `cache_entry_` must outlive `font_stream_`. Faces managed by a cache
   // and sharing the same `font_stream_` keep the cache entry that indexes
   // that stream alive via this member while there is at least one face
@@ -210,10 +199,10 @@ class CFX_Face final : public Retainable, public Observable {
   ScopedFXFTFaceRec const rec_;
 #if defined(PDF_USE_SKIA)
   sk_sp<SkTypeface> skia_typeface_;
-#endif  // defined(PDF_USE_SKIA)
 #if defined(PDF_ENABLE_FONTATIONS)
-  std::unique_ptr<SkrifaFontHolder> skrifa_font_;
-#endif
+  std::unique_ptr<SkrifaFontHolder> const skrifa_font_;
+#endif  // defined(PDF_ENABLE_FONTATIONS)
+#endif  // defined(PDF_USE_SKIA)
 };
 
 #endif  // CORE_FXGE_CFX_FACE_H_
