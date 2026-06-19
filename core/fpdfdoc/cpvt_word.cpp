f91482487ddd7882911c55dedaec91d9dfdbe103
Seung Hyun Jin <seunghyunjin@google.com>
2026-06-19 00:07:14 +0000

    Rename CPVT_Word member variables
    Update private member variables by removing Hungarian notation and
    appending trailing underscores.
    
    Bug: 40115028
    Change-Id: I378b5ab094949eb299b1d3796e8ca5f73052032c

--- core/fpdfdoc/cpvt_word.cpp
+++ core/fpdfdoc/cpvt_word.cpp
@@ -5,6 +5,24 @@
 #include "core/fpdfdoc/cpvt_word.h"
 
 CPVT_Word::CPVT_Word() = default;
+
+CPVT_Word::CPVT_Word(uint16_t word,
+                     FX_Charset charset,
+                     const CFX_PointF& location,
+                     float ascent,
+                     float descent,
+                     float width,
+                     int32_t font_index,
+                     float font_size)
+    : word_(word),
+      charset_(charset),
+      location_(location),
+      ascent_(ascent),
+      descent_(descent),
+      width_(width),
+      font_index_(font_index),
+      font_size_(font_size) {}
+
 CPVT_Word::CPVT_Word(const CPVT_Word&) = default;
 CPVT_Word& CPVT_Word::operator=(const CPVT_Word&) = default;
 CPVT_Word::~CPVT_Word() = default;
--- core/fpdfdoc/cpvt_word.h
+++ core/fpdfdoc/cpvt_word.h
@@ -9,26 +9,46 @@
 
 #include <stdint.h>
 
-#include "core/fpdfdoc/cpvt_wordplace.h"
 #include "core/fxcrt/fx_codepage.h"
 #include "core/fxcrt/fx_coordinates.h"
 
 class CPVT_Word {
  public:
   CPVT_Word();
+  CPVT_Word(uint16_t word,
+            FX_Charset charset,
+            const CFX_PointF& location,
+            float ascent,
+            float descent,
+            float width,
+            int32_t font_index,
+            float font_size);
   CPVT_Word(const CPVT_Word&);
   CPVT_Word& operator=(const CPVT_Word&);
   ~CPVT_Word();
 
-  uint16_t Word = 0;
-  FX_Charset nCharset = FX_Charset::kANSI;
-  CPVT_WordPlace WordPlace;
-  CFX_PointF ptWord;
-  float fAscent = 0.0f;
-  float fDescent = 0.0f;
-  float fWidth = 0.0f;
-  int32_t nFontIndex = -1;
-  float fFontSize = 0.0f;
+  uint16_t word() const { return word_; }
+  FX_Charset charset() const { return charset_; }
+  int32_t font_index() const { return font_index_; }
+  const CFX_PointF& location() const { return location_; }
+  void set_location(const CFX_PointF& location) { location_ = location; }
+  float width() const { return width_; }
+  float font_size() const { return font_size_; }
+  float ascent() const { return ascent_; }
+  float descent() const { return descent_; }
+
+  float AscentY() const { return location_.y + ascent_; }
+  float DescentY() const { return location_.y + descent_; }
+
+ private:
+  uint16_t word_ = 0;
+  FX_Charset charset_ = FX_Charset::kANSI;
+  CFX_PointF location_;
+  float ascent_ = 0.0f;
+  float descent_ = 0.0f;
+  float width_ = 0.0f;
+  int32_t font_index_ = -1;
+  float font_size_ = 0.0f;
 };
 
 #endif  // CORE_FPDFDOC_CPVT_WORD_H_
