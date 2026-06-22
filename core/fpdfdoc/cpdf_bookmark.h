fc5cb5a993d41fa45bc3fc5ce983a9b3887c8fe6
Aryan <aryankrishnan4b@gmail.com>
2026-06-22 23:11:27 +0530

    Add FPDFBookmark_GetColor public API
    This CL Introduces a new public API function, FPDFBookmark_GetColor,
    which allows emedders to retrieve the RGB color components of a PDF
    outline item (bookmark).
    
    To support this, the CPDF_Bookmark::GetColor() method has been added to
    get values from the optional "C" array from the underlying bookmark
    dictionary. The API validates that the RGB color components are present,
    correctly formed, and conform to the PDF specification limits (values
    between 0.0 and 1.0).
    
    Bug: 362787705
    Change-Id: I246243c32c7c4b35a7f4200b610712282e66b35a

--- core/fpdfdoc/cpdf_bookmark.h
+++ core/fpdfdoc/cpdf_bookmark.h
@@ -11,9 +11,15 @@
 #include "core/fpdfdoc/cpdf_dest.h"
 #include "core/fxcrt/retain_ptr.h"
 #include "core/fxcrt/widestring.h"
+#include "core/fxge/dib/fx_dib.h"
 
 class CPDF_Dictionary;
 class CPDF_Document;
+
+struct ColorReturn {
+  bool success;
+  FX_RGB_STRUCT<float> color;
+}
 
 class CPDF_Bookmark {
  public:
@@ -27,6 +33,7 @@ class CPDF_Bookmark {
   WideString GetTitle() const;
   CPDF_Dest GetDest(CPDF_Document* document) const;
   CPDF_Action GetAction() const;
+  ColorReturn CPDF_Bookmark::GetColor() const;
   int GetCount() const;
 
  private:
