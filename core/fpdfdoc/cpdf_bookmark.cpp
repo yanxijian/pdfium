58d64dad944404f5fbddf9f93e99c836b12d5672
Aryan <aryankrishnan4b@gmail.com>
2026-06-23 01:23:57 +0530

    Add FPDFBookmark_GetColor public API
    This CL introduces a new public API function, FPDFBookmark_GetColor,
    which allows emedders to retrieve the RGB color components of a PDF
    outline item (bookmark).
    
    To support this, This CL adds the CPDF_Bookmark::GetColor() method to
    get values from the optional "/C" array from the underlying bookmark
    dictionary. The API validates that the RGB color components are present,
    correctly formed, and conform to the PDF specification limits (values
    between 0.0 and 1.0).
    
    Bug: 362787705
    Change-Id: I246243c32c7c4b35a7f4200b610712282e66b35a

--- core/fpdfdoc/cpdf_bookmark.cpp
+++ core/fpdfdoc/cpdf_bookmark.cpp
@@ -11,7 +11,9 @@
 
 #include "core/fpdfapi/parser/cpdf_array.h"
 #include "core/fpdfapi/parser/cpdf_dictionary.h"
+#include "core/fpdfapi/parser/cpdf_number.h"
 #include "core/fpdfapi/parser/cpdf_string.h"
+#include "core/fxcrt/retain_ptr.h"
 #include "core/fxge/dib/fx_dib.h"
 
 CPDF_Bookmark::CPDF_Bookmark() = default;
@@ -50,6 +52,20 @@ CPDF_Dest CPDF_Bookmark::GetDest(CPDF_Document* document) const {
 
 CPDF_Action CPDF_Bookmark::GetAction() const {
   return CPDF_Action(dict_ ? dict_->GetDictFor("A") : nullptr);
+}
+
+std::optional<FX_RGB_STRUCT<float>> CPDF_Bookmark::GetColor() const {
+  if (!dict_) {
+    return std::nullopt;
+  }
+  RetainPtr<const CPDF_Array> RGB_array = dict_->GetArrayFor("C");
+  if (!RGB_array || RGB_array->size() != 3) {
+    return std::nullopt;
+  }
+  float red = RGB_array->GetNumberAt(0)->GetNumber();
+  float green = RGB_array->GetNumberAt(1)->GetNumber();
+  float blue = RGB_array->GetNumberAt(2)->GetNumber();
+  return {{red, green, blue}};
 }
 
 int CPDF_Bookmark::GetCount() const {
