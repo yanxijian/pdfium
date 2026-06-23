745777d8582673c286f7613c64a3547a189bf830
Wang Yongjie <tycket033@gmail.com>
2026-06-23 23:59:56 +0800

    Add timezone offset to new document creation date
    FPDF_CreateNewDocument() writes CreationDate using the local time, but
    omitted the UTC offset. Include the offset in the PDF date string when
    machine time is available.
    
    Add a deterministic regression test for a non-hour timezone offset and
    update the saved document expectation for the longer date string.
    
    Bug: 525699558
    Change-Id: I8dc9100199438350bd0f7f1f33b7aa07efefb1ef

--- core/fxcrt/fx_extension.h
+++ core/fxcrt/fx_extension.h
@@ -12,6 +12,7 @@
 #include <time.h>
 #include <wctype.h>
 
+#include "core/fxcrt/autorestorer.h"
 #include "core/fxcrt/span.h"
 #include "core/fxcrt/widestring.h"
 
@@ -152,5 +153,43 @@ void FXSYS_SetLocaltimeFunction(struct tm* (*func)(const time_t*));
 // Replacements for time/localtime that respect overrides.
 time_t FXSYS_time(time_t* tloc);
 struct tm* FXSYS_localtime(const time_t* tp);
+
+int FXSYS_TimeZoneOffsetInMinutes(const tm& local_time, const tm& utc_time);
+
+namespace fxcrt {
+
+using TimeFunction = time_t (*)();
+using LocaltimeFunction = struct tm* (*)(const time_t*);
+
+class ScopedTimeFunction {
+ public:
+  FX_STACK_ALLOCATED();
+
+  explicit ScopedTimeFunction(TimeFunction func);
+  ScopedTimeFunction(const ScopedTimeFunction&) = delete;
+  ScopedTimeFunction& operator=(const ScopedTimeFunction&) = delete;
+  ~ScopedTimeFunction();
+
+ private:
+  AutoRestorer<TimeFunction> restorer_;
+};
+
+class ScopedLocaltimeFunction {
+ public:
+  FX_STACK_ALLOCATED();
+
+  explicit ScopedLocaltimeFunction(LocaltimeFunction func);
+  ScopedLocaltimeFunction(const ScopedLocaltimeFunction&) = delete;
+  ScopedLocaltimeFunction& operator=(const ScopedLocaltimeFunction&) = delete;
+  ~ScopedLocaltimeFunction();
+
+ private:
+  AutoRestorer<LocaltimeFunction> restorer_;
+};
+
+}  // namespace fxcrt
+
+using fxcrt::ScopedLocaltimeFunction;
+using fxcrt::ScopedTimeFunction;
 
 #endif  // CORE_FXCRT_FX_EXTENSION_H_
