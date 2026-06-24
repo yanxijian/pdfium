df4b2b7d02b42365fa393d8f842164418d3add13
Seung Hyun Jin <seunghyunjin@google.com>
2026-06-24 20:44:11 +0000

    Support pure RTL text in form field through CPVT_section
    Add `is_rtl` boolean to CPVT_WordInfo and CPVT_Word to track text
    direction. Calculate x-coords with CFX_BidiChar for RTL chars in
    CPVT_Section. Update CPVT_Word::CaretX() and GetLineCaretX() to
    use the `is_rtl` for correct caret positioning. Added unit
    test to verify RTL caret coordinates.
    
    Bug: 40115028
    Change-Id: Ie15c1d6a6519cf3878bc625aaa74c67bf6681b9d

--- core/fpdfdoc/cpvt_variabletext.cpp
+++ core/fpdfdoc/cpvt_variabletext.cpp
@@ -99,6 +99,19 @@ void CPVT_VariableText::Iterator::SetAt(const CPVT_WordPlace& place) {
   cur_pos_ = place;
 }
 
+float CPVT_VariableText::Iterator::GetLineCaretX(const CPVT_Line& line) {
+  bool is_rtl = false;
+  CPVT_WordPlace old_place = GetWordPlace();
+  if (NextWord()) {
+    CPVT_Word first_word;
+    if (GetWord(first_word) && first_word.is_rtl()) {
+      is_rtl = true;
+    }
+  }
+  SetAt(old_place);
+  return is_rtl ? line.ptLine.x + line.fLineWidth : line.ptLine.x;
+}
+
 bool CPVT_VariableText::Iterator::NextWord() {
   if (cur_pos_ == vt_->GetEndWordPlace()) {
     return false;
@@ -146,7 +159,8 @@ bool CPVT_VariableText::Iterator::GetWord(CPVT_Word& word) const {
       vt_->InToOut(CFX_PointF(pInfo->fWordX + pSection->GetRect().left,
                               pInfo->fWordY + pSection->GetRect().top)),
       vt_->GetWordAscent(*pInfo), vt_->GetWordDescent(*pInfo),
-      vt_->GetWordWidth(*pInfo), pInfo->nFontIndex, vt_->GetFontSize());
+      vt_->GetWordWidth(*pInfo), pInfo->nFontIndex, vt_->GetFontSize(),
+      pInfo->is_rtl);
   return true;
 }
 
