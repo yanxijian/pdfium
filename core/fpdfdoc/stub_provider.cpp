84f21880062389370ae22649e8aee99ed8af13db
Lei Zhang <thestig@chromium.org>
2026-06-25 18:29:43 -0700

    Add some unit tests for CPVT_Section
    Add tests to make sure CPVT_Section::ClearWords() works correctly.
    Extract the existing StubProvider in cpvt_variabletext_unittest.cpp into
    its own file for reuse in the new test file.
    
    TAG=agy
    CONV=68ba6199-ece1-4ef9-a9b2-22b544af3ab9
    
    Change-Id: I2ab8313eb53b6b43238565c44f9550f6d8f48bee

---
+++ core/fpdfdoc/stub_provider.cpp
@@ -0,0 +1,32 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/fpdfdoc/stub_provider.h"
+
+StubProvider::StubProvider(IPVT_FontMap* font_map)
+    : CPVT_VariableText::Provider(font_map) {}
+
+StubProvider::~StubProvider() = default;
+
+int StubProvider::GetCharWidth(int32_t nFontIndex, uint16_t word) {
+  return 10;
+}
+
+int32_t StubProvider::GetTypeAscent(int32_t nFontIndex) {
+  return 10;
+}
+
+int32_t StubProvider::GetTypeDescent(int32_t nFontIndex) {
+  return -2;
+}
+
+int32_t StubProvider::GetWordFontIndex(uint16_t word,
+                                       FX_Charset charset,
+                                       int32_t nFontIndex) {
+  return 0;
+}
+
+int32_t StubProvider::GetDefaultFontIndex() {
+  return 0;
+}
