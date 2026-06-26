de51e6634febbdadac57b72245874845e30e612a
Seung Hyun Jin <seunghyunjin@google.com>
2026-06-26 00:49:05 +0000

    Support pure RTL text in form field through CPVT_section
    Calculate x-coords with CFX_BidiChar for RTL chars in CPVT_Section.
    Update unit tests to verify RTL word layout coordinates. Temporarily
    updated expected Hebrew pixel test images to reflect intermediate
    rendering output before visual rendering changes connected to AP stream
    corrections in appended CL.
    
    Bug: 40115028
    Change-Id: Ie15c1d6a6519cf3878bc625aaa74c67bf6681b9d

--- core/fpdfdoc/cpvt_section.cpp
+++ core/fpdfdoc/cpvt_section.cpp
@@ -12,6 +12,7 @@
 #include "core/fpdfdoc/cpvt_variabletext.h"
 #include "core/fpdfdoc/cpvt_wordinfo.h"
 #include "core/fxcrt/check.h"
+#include "core/fxcrt/fx_bidi.h"
 #include "core/fxcrt/stl_util.h"
 
 namespace {
@@ -716,14 +717,39 @@ CPVT_FloatRect CPVT_Section::OutputLines(const CPVT_FloatRect& rect) const {
       fPosY += pLine->line_info_.fLineAscent;
       pLine->line_info_.fLineX = fPosX - fMinX;
       pLine->line_info_.fLineY = fPosY - fMinY;
+      WideString line_str;
       for (int32_t w = pLine->line_info_.nBeginWordIndex;
            w <= pLine->line_info_.nEndWordIndex; w++) {
         if (fxcrt::IndexInBounds(word_array_, w)) {
-          CPVT_WordInfo* pWord = word_array_[w].get();
-          pWord->fWordX = fPosX - fMinX;
-          pWord->fWordY = fPosY - fMinY;
+          line_str += word_array_[w]->Word;
+        }
+      }
 
-          fPosX += vt_->GetWordWidth(*pWord);
+      CFX_BidiString bidi(line_str);
+      const bool is_overall_direction_right =
+          bidi.OverallDirection() == CFX_BidiChar::Direction::kRight;
+
+      for (const auto& segment : bidi) {
+        bool is_rtl =
+            (segment.direction == CFX_BidiChar::Direction::kRight ||
+             (segment.direction == CFX_BidiChar::Direction::kNeutral &&
+              is_overall_direction_right));
+
+        int32_t start =
+            is_rtl ? segment.start + segment.count - 1 : segment.start;
+        int32_t end =
+            is_rtl ? segment.start - 1 : segment.start + segment.count;
+        int32_t step = is_rtl ? -1 : 1;
+
+        for (int32_t i = start; i != end; i += step) {
+          int32_t w = pLine->line_info_.nBeginWordIndex + i;
+          if (fxcrt::IndexInBounds(word_array_, w)) {
+            CPVT_WordInfo* word_info = word_array_[w].get();
+            word_info->fWordX = fPosX - fMinX;
+            word_info->fWordY = fPosY - fMinY;
+            word_info->is_rtl = is_rtl;
+            fPosX += vt_->GetWordWidth(*word_info);
+          }
         }
       }
       fPosY -= pLine->line_info_.fLineDescent;
