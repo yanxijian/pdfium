197020a98b5151e65f38e4fde9c5f86a939ec293
Seung Hyun Jin <seunghyunjin@google.com>
2026-06-29 19:03:52 +0000

    Caret calculation for RTL text in CPWL_Edit
    Update `CaretX()` to align the caret on left edge of RTL chars.
    Introduces GetLineCaretX() to determine correct starting caret coord
    for entire line based on first word's direction.
    
    Bug: 40115028
    Change-Id: I7e5be16d9897f36849484d71b7e6656dd052d573

--- fpdfsdk/pwl/cpwl_edit_impl.cpp
+++ fpdfsdk/pwl/cpwl_edit_impl.cpp
@@ -15,6 +15,7 @@
 #include "core/fpdfapi/render/cpdf_renderoptions.h"
 #include "core/fpdfapi/render/cpdf_textrenderer.h"
 #include "core/fpdfdoc/cpvt_word.h"
+#include "core/fpdfdoc/cpvt_wordinfo.h"
 #include "core/fpdfdoc/ipvt_fontmap.h"
 #include "core/fxcrt/autorestorer.h"
 #include "core/fxcrt/check.h"
@@ -91,6 +92,10 @@ void CPWL_EditImpl::Iterator::SetAt(const CPVT_WordPlace& place) {
 
 const CPVT_WordPlace& CPWL_EditImpl::Iterator::GetAt() const {
   return vt_iterator_->GetWordPlace();
+}
+
+float CPWL_EditImpl::Iterator::GetLineCaretX(const CPVT_Line& line) {
+  return vt_iterator_->GetLineCaretX(line);
 }
 
 class CPWL_EditImpl::Provider final : public CPVT_VariableText::Provider {
@@ -1236,7 +1241,7 @@ void CPWL_EditImpl::ScrollToCaret() {
     ptFoot.x = ptHead.x;
     ptFoot.y = word.DescentY();
   } else if (pIterator->GetLine(line)) {
-    ptHead.x = line.ptLine.x;
+    ptHead.x = pIterator->GetLineCaretX(line);
     ptHead.y = line.ptLine.y + line.fLineAscent;
     ptFoot.x = ptHead.x;
     ptFoot.y = line.ptLine.y + line.fLineDescent;
@@ -1404,7 +1409,7 @@ void CPWL_EditImpl::SetCaretInfo() {
         ptFoot.x = ptHead.x;
         ptFoot.y = word.DescentY();
       } else if (pIterator->GetLine(line)) {
-        ptHead.x = line.ptLine.x;
+        ptHead.x = pIterator->GetLineCaretX(line);
         ptHead.y = line.ptLine.y + line.fLineAscent;
         ptFoot.x = ptHead.x;
         ptFoot.y = line.ptLine.y + line.fLineDescent;
