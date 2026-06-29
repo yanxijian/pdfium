c35d500967be37d30e3df3ff21eb3be7bfb2ee80
Seung Hyun Jin <seunghyunjin@google.com>
2026-06-29 23:29:26 +0000

    Caret calculation for RTL text in CPWL_Edit
    Update `CaretX()` to align the caret on left edge of RTL chars.
    Introduces GetLineCaretX() to determine correct starting caret coord
    for entire line based on first word's direction.
    
    Bug: 40115028
    Change-Id: I7e5be16d9897f36849484d71b7e6656dd052d573

--- core/fpdfdoc/cpvt_variabletext.cpp
+++ core/fpdfdoc/cpvt_variabletext.cpp
@@ -99,6 +99,19 @@ void CPVT_VariableText::Iterator::SetAt(const CPVT_WordPlace& place) {
   cur_pos_ = place;
 }
 
+float CPVT_VariableText::Iterator::GetLineCaretX(const CPVT_Line& line) {
+  bool is_rtl = false;
+  CPVT_WordPlace old_place = GetWordPlace();
+  if (NextWord()) {
+    CPVT_Word first_word;
+    if (GetWord(first_word) && first_word.is_rtl()) {
+      is_rtl = true;
+    }
+  }
+  SetAt(old_place);
+  return is_rtl ? line.ptLine.x + line.fLineWidth : line.ptLine.x;
+}
+
 bool CPVT_VariableText::Iterator::NextWord() {
   if (cur_pos_ == vt_->GetEndWordPlace()) {
     return false;
