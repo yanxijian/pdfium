668f5ed87df61033688864523277d6490454a6eb
Aryan <aryankrishnan4b@gmail.com>
2026-06-30 16:26:55 +0530

    Add configuration option to control Brotli decoding
    This CL updates FPDF_LIBRARY_CONFIG to version 6, introducing an
    experimental m_BrotliEnabled flag. This allows applications to
    dynamically enable or disable /BrotliDecode at library initialization
    time when PDFium is compiled with Brotli support. Other codepaths check
    if Brotli is enabled before decoding brotli-encoded streams.
    
    Bug: 475855993
    Change-Id: I4ef555dfc58581bae0289c293d2f77a387749637

--- core/fxcodec/brotli/brotli_embeddertest.cpp
+++ core/fxcodec/brotli/brotli_embeddertest.cpp
@@ -2,13 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "core/fxge/cfx_gemodule.h"
+#include "public/fpdfview.h"
 #include "testing/embedder_test.h"
 #include "testing/embedder_test_constants.h"
+#include "testing/embedder_test_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using BrotliEmbedderTest = EmbedderTest;
 
+namespace {
+EmbedderTestEnvironment* env;
+}  // namespace
+
 TEST_F(BrotliEmbedderTest, ManyRectanglesBrotli) {
+  env = EmbedderTestEnvironment::GetInstance();
+  FPDF_DestroyLibrary();
+  env->SetUpBrotli(/*brotli=*/true);
   ASSERT_TRUE(OpenDocument("many_rectangles_brotli.pdf"));
 
   ScopedPage page = LoadScopedPage(0);
@@ -21,6 +31,9 @@ TEST_F(BrotliEmbedderTest, ManyRectanglesBrotli) {
 }
 
 TEST_F(BrotliEmbedderTest, SimpleBrotliWithText) {
+  env = EmbedderTestEnvironment::GetInstance();
+  FPDF_DestroyLibrary();
+  env->SetUpBrotli(/*brotli=*/true);
   ASSERT_TRUE(OpenDocument("hello_world_brotli.pdf"));
 
   ScopedPage page = LoadScopedPage(0);
@@ -33,6 +46,9 @@ TEST_F(BrotliEmbedderTest, SimpleBrotliWithText) {
 }
 
 TEST_F(BrotliEmbedderTest, BrotliRectangles) {
+  env = EmbedderTestEnvironment::GetInstance();
+  FPDF_DestroyLibrary();
+  env->SetUpBrotli(/*brotli=*/true);
   ASSERT_TRUE(OpenDocument("rectangles_brotli.pdf"));
 
   ScopedPage page = LoadScopedPage(0);
@@ -45,6 +61,9 @@ TEST_F(BrotliEmbedderTest, BrotliRectangles) {
 }
 
 TEST_F(BrotliEmbedderTest, BrotliWithLength1Argument) {
+  env = EmbedderTestEnvironment::GetInstance();
+  FPDF_DestroyLibrary();
+  env->SetUpBrotli(/*brotli=*/true);
   ASSERT_TRUE(OpenDocument("hello_world_brotli_with_length1.pdf"));
 
   ScopedPage page = LoadScopedPage(0);
@@ -54,4 +73,19 @@ TEST_F(BrotliEmbedderTest, BrotliWithLength1Argument) {
   ASSERT_TRUE(bitmap);
 
   CompareBitmapWithExpectationSuffix(bitmap.get(), pdfium::kHelloWorldPng);
+}
+
+TEST_F(BrotliEmbedderTest, BrotliDecodeDisabled) {
+  env = EmbedderTestEnvironment::GetInstance();
+  FPDF_DestroyLibrary();
+  env->SetUpBrotli(/*brotli=*/false);
+  ASSERT_TRUE(OpenDocument("hello_world_brotli.pdf"));
+
+  ScopedPage page = LoadScopedPage(0);
+  ASSERT_TRUE(page);
+
+  ScopedFPDFBitmap bitmap = RenderLoadedPage(page.get());
+  ASSERT_TRUE(bitmap);
+
+  CompareBitmap(bitmap.get(), "hello_world_brotli_disabled");
 }
