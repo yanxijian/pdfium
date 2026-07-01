8f5bef49750802c538ac1e40f1a03abd80d23628
Tom Sepez <tsepez@google.com>
2026-07-01 22:34:49 +0000

    Fix unsafe buffers in CompositeRow_Rgb2Mask().
    Use fxcrt::Zip() over spans.
    
    Change-Id: I3a49f329c1249d21d84f771a43a8e04a6e5a4b81

--- core/fxge/dib/cfx_scanlinecompositor.cpp
+++ core/fxge/dib/cfx_scanlinecompositor.cpp
@@ -199,21 +199,17 @@ void CompositeRowBgra2Mask(pdfium::span<const FX_BGRA_STRUCT<uint8_t>> src_span,
 }
 
 void CompositeRow_Rgb2Mask(pdfium::span<uint8_t> dest_span,
-                           int width,
+                           size_t width,
                            pdfium::span<const uint8_t> clip_span) {
+  auto dest_sub = dest_span.first(width);
   if (clip_span.empty()) {
-    std::ranges::fill(dest_span.first(static_cast<size_t>(width)), 0xff);
+    std::ranges::fill(dest_sub, 0xff);
     return;
   }
-  uint8_t* dest_scan = dest_span.data();
-  const uint8_t* clip_scan = clip_span.data();
-  UNSAFE_TODO({
-    for (int i = 0; i < width; ++i) {
-      *dest_scan = AlphaUnion(*dest_scan, *clip_scan);
-      ++dest_scan;
-      ++clip_scan;
-    }
-  });
+  auto clip_sub = clip_span.first(width);
+  for (auto [clip, output] : fxcrt::Zip(clip_sub, dest_sub)) {
+    output = AlphaUnion(output, clip);
+  }
 }
 
 bool IsNonSeparableBlendMode(BlendMode mode) {
@@ -2137,7 +2133,7 @@ void CFX_ScanlineCompositor::CompositeRgbBitmapLineSrcBgrx(
     }
     case FXDIB_Format::k8bppMask: {
       CHECK(!rgb_byte_order_);  // Disallowed by Init();
-      CompositeRow_Rgb2Mask(dest_scan, width, clip_scan);
+      CompositeRow_Rgb2Mask(dest_scan, static_cast<size_t>(width), clip_scan);
       return;
     }
     case FXDIB_Format::kBgr:
@@ -2407,7 +2403,7 @@ void CFX_ScanlineCompositor::CompositePalBitmapLineSrcBpp1(
     }
     case FXDIB_Format::k8bppMask: {
       CHECK(!rgb_byte_order_);  // Disallowed by Init();
-      CompositeRow_Rgb2Mask(dest_scan, width, clip_scan);
+      CompositeRow_Rgb2Mask(dest_scan, static_cast<size_t>(width), clip_scan);
       return;
     }
     case FXDIB_Format::kBgr:
@@ -2468,7 +2464,7 @@ void CFX_ScanlineCompositor::CompositePalBitmapLineSrcBpp8(
     }
     case FXDIB_Format::k8bppMask: {
       CHECK(!rgb_byte_order_);  // Disallowed by Init();
-      CompositeRow_Rgb2Mask(dest_scan, width, clip_scan);
+      CompositeRow_Rgb2Mask(dest_scan, static_cast<size_t>(width), clip_scan);
       return;
     }
     case FXDIB_Format::kBgr:
