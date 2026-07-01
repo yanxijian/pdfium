29d75beef194e1e31045bd6eefb19d203601899f
Tom Sepez <tsepez@google.com>
2026-07-01 17:05:39 +0000

    De-templatize StringPoolTemplate into concrete ByteStringPool
    Replace StringPoolTemplate<ByteString> with a concrete ByteStringPool
    class and remove StringPoolTemplate<WideString> which is unused across
    the entire codebase.
    
    -- Remove unused Clear() method.
    
    TAG=agy
    CONV=6c5ab994-4015-47b7-b75b-b0a21929935f
    
    Bypass-Check-License: moved files.
    Change-Id: I6ca43c8a7fe1381c7660c3dcfaf27d02fa680d1c

--- core/fpdfdoc/cpdf_filespec.h
+++ core/fpdfdoc/cpdf_filespec.h
@@ -7,8 +7,8 @@
 #ifndef CORE_FPDFDOC_CPDF_FILESPEC_H_
 #define CORE_FPDFDOC_CPDF_FILESPEC_H_
 
+#include "core/fxcrt/bytestring_pool.h"
 #include "core/fxcrt/retain_ptr.h"
-#include "core/fxcrt/string_pool_template.h"
 #include "core/fxcrt/weak_ptr.h"
 #include "core/fxcrt/widestring.h"
 
