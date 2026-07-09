b719bf210c1d35478addab642789ad76c2f7551a
Aryan <aryankrishnan4b@gmail.com>
2026-07-10 01:01:21 +0530

    Fix banding issues with /ICCBased gradients
    This CL updates the cache to not use the quantized values to create
    smoother gradients and resolve the banding in /ICCBased gradients.
    
    Bug: 532617608
    Change-Id: I0c749b43b09e1740f800531fcf055ce352f40418

--- core/fpdfapi/page/cpdf_colorspace.cpp
+++ core/fpdfapi/page/cpdf_colorspace.cpp
@@ -1016,8 +1016,9 @@ void CPDF_ICCBasedCS::TranslateImageLine(pdfium::span<uint8_t> dest_span,
   const uint32_t nComponents = ComponentCount();
   DCHECK(fxcodec::IccTransform::IsValidIccComponents(nComponents));
   int nMaxColors = 1;
+  constexpr int kColorMaxValue = 256;
   for (uint32_t i = 0; i < nComponents; i++) {
-    nMaxColors *= 52;
+    nMaxColors *= kColorMaxValue;
   }
 
   bool bTranslate = nComponents > 3;
@@ -1038,11 +1039,11 @@ void CPDF_ICCBasedCS::TranslateImageLine(pdfium::span<uint8_t> dest_span,
     size_t src_index = 0;
     for (int i = 0; i < nMaxColors; i++) {
       uint32_t color = i;
-      uint32_t order = nMaxColors / 52;
+      uint32_t order = nMaxColors / kColorMaxValue;
       for (uint32_t c = 0; c < nComponents; c++) {
-        temp_src[src_index++] = static_cast<uint8_t>(color / order * 5);
+        temp_src[src_index++] = static_cast<uint8_t>(color / order);
         color %= order;
-        order /= 52;
+        order /= kColorMaxValue;
       }
     }
     if (profile_->IsSupported()) {
@@ -1055,7 +1056,7 @@ void CPDF_ICCBasedCS::TranslateImageLine(pdfium::span<uint8_t> dest_span,
     for (int i = 0; i < pixels; i++) {
       int index = 0;
       for (uint32_t c = 0; c < nComponents; c++) {
-        index = index * 52 + (*pSrcBuf) / 5;
+        index = index * kColorMaxValue + (*pSrcBuf);
         pSrcBuf++;
       }
       index *= 3;
